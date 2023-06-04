01c6c9bb80504a4442ee299cb2260dc16c3e8b7f
moloudii <mhmmdalinaz@gmail.com>
2023-06-04 11:09:45 +0330

    generate store in js

---
+++ redux-js.js
@@ -0,0 +1,28 @@
+const createStore = (reducer) => {
+  let state;
+
+  dispatch = (action) => {
+    state = reducer(state, action);
+  };
+  dispatch({});
+  const getState = () => state;
+  return { dispatch, getState };
+};
+
+function counter(state = 0, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+const store = createStore(counter);
+console.log(store.getState());
+store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "INCREMENT" });
+
+console.log(store.getState());
